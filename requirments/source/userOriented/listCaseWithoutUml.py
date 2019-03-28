3a4d30158fe09ab0736d33bc9e4118cbaca0174b
LiuHanCheng <2463765697@qq.com>
2019-03-28 23:18:20 +0800

    SHADOC-002 DOC UPDATE
    AUTHOR:CNLHC
    CENSOR:CNLHC
    NOTE: 1. 在"业务需求"一节中添加系统用例图 2. 修改了文档中图片的索引标题

--- requirments/source/userOriented/listCaseWithoutUml.py
+++ requirments/source/userOriented/listCaseWithoutUml.py
@@ -1,6 +1,6 @@
 import os
 import json
-with open('./case.json','r') as fp: 
+with open('./case.json','rb') as fp: 
     jsobj=json.load(fp)
     nameCaseList = set([case['name'] for case in jsobj])
     umlList = set([i.split('.')[0] for i in os.listdir('./uml')])
