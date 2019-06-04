0d4b190cb7d91498448fd7cf6ad0de5a302af910
CNLHC <2463765697@qq.com>
2019-06-04 09:14:41 +0800

    dev

--- testReport/source/6.testAnalysis/analysis.py
+++ testReport/source/6.testAnalysis/analysis.py
@@ -1,10 +1,12 @@
 import json
 import requests
 import functools
+import hashlib
 
         
 requirementsObj = json.load(open('../../../requirments/source/userOriented/case.json'))
 regressionObj = json.load(open('../4.testCase/regression.json','r'))
+unittestObj = json.load(open('../4.testCase/unittest.json','r'))
 
 
 
@@ -16,7 +18,39 @@ def f(x,y):
 a=functools.reduce(f,[i['cases'] for i in regressionObj],[])
 b = [i['name'] for i in requirementsObj]
 
-print(a)
-print(b)
 
 
+# print(len(a))
+# print(b)
+# print(len(b))
+# print(len(unittestObj))
+# print(len(regressionObj))
+
+
+
+ctxs=regressionObj
+template="+ :ref:`{testType}:{name}`: {cases}"
+
+
+with open('./uncorvered.gen.rstinc','w') as fp:
+    uncov = (set(b)-set(a))
+    fp.write("\n".join(
+        ["+ :ref:`用例:{name}`".format(name=i) for i in uncov]
+        
+        ))
+
+
+
+
+with open('./RegressionCases.gen.rstinc','w') as fp:
+    for  ctx in ctxs:
+        ctx['testType'] = "回归测试用例"
+
+        ctx['cases'] =" , ".join([":ref:`用例:{name}`".format(name=i) for i in ctx['cases']])
+        m = hashlib.sha256()
+        m.update(bytes(ctx['name'],'utf-8'))
+        
+        ctx['hash']  =  m.hexdigest()[0:5]
+
+        fp.write(template.format(**ctx))
+        fp.write("\n")
