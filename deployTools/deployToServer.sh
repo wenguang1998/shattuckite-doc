8b916d0726c266aee15c05f2b8a3e20831d843b8
CNLHC <2463765697@qq.com>
2019-06-09 01:57:16 +0800

    SHADOC META UPDATE
    AUTHOR:CNLHC
    CENSOR:CNLHC
    NOTE:  1. 更新CMake文件，修改需求文档简写 2. 添加CI
    Git部署脚本并添加相关Stage

--- deployTools/deployToServer.sh
+++ deployTools/deployToServer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rsync -avPog --chown=www-data:www-data -e ssh ./build/SDP-html deploy@ali.cnworkshop.xyz:/var/www/shattuckite-doc;
-rsync -avPog --chown=www-data:www-data -e ssh ./build/PRD-html deploy@ali.cnworkshop.xyz:/var/www/shattuckite-doc;
+rsync -avPog --chown=www-data:www-data -e ssh ./build/SRS-html deploy@ali.cnworkshop.xyz:/var/www/shattuckite-doc;
 rsync -avPog --chown=www-data:www-data -e ssh ./build/SDD-html deploy@ali.cnworkshop.xyz:/var/www/shattuckite-doc;
+rsync -avPog --chown=www-data:www-data -e ssh ./build/STR-html deploy@ali.cnworkshop.xyz:/var/www/shattuckite-doc;
